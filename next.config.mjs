f9c52a32dcc299049ebaa9f1f67dca7148e6f303
Iat Hou Fong <ifong6@gatech.edu>
2024-06-05 10:55:21 -0700

    remove

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["s3.us-west-2.amazonaws.com"],
-  },
-};
-
-export default nextConfig;
--- src/app/account/personal-info/style.js
+++
@@ -1,5 +0,0 @@
-export const styles = {
-    profileImage: "w-24 h-24 rounded-full overflow-hidden",
-    buttonSignOut: "mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
-    buttonUploadFile: "mt-2 bg-gray-400 hover:bg-gray-500 text-white py-1 px-3 rounded-full cursor-pointer text-xs",
-};
